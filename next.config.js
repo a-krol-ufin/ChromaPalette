9df0f0a0a4258d52dd1a9755a586127bc1a60d99
a-krol-ufin <a.krol@ufin.pl>
2025-06-18 08:09:58 +0200

    Configures static export
    Configures the Next.js application for static export.
    
    This includes enabling `output: 'export'` and `trailingSlash: true`
    for compatibility with static hosting environments. Also sets
    `unoptimized: true` to bypass image optimization during export.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
     images: {
         domains: ['images.unsplash.com'],
+        unoptimized: true
     }
 };
 
